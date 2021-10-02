f81194173e9a83d628d4f2541c370faaf8d9be2d
Muhammad Firmansyah Rifai <mrifai@thebigrichgroup.com>
2021-10-02 12:00:15 +0700

    Create Auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('roles')->nullable();
+            $table->boolean('confirmed')->nullable()->default(false);
+            $table->string('profile_pic_name')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
